907157475dad1133526bccf0ed1f01835d2530a2
John Ayers <ayersprogramming@gmail.com>
2018-05-10 15:04:33 -0500

    Added user location functionality

--- MGRS Converter/MapViewController.swift
+++ MGRS Converter/MapViewController.swift
@@ -14,15 +14,21 @@ class MapViewController: UIViewController, UIGestureRecognizerDelegate, MKMapVie
     @IBOutlet var mapView: MKMapView!
     @IBOutlet var mgrsLabel: UILabel!
     @IBOutlet var latLonLabel: UILabel!
+    @IBOutlet var showLocationButton: UIButton!
+    @IBOutlet var clearMapButton: UIButton!
+//    var locationManager: CLLocationManager!
     
     let mapPin = MKPointAnnotation()
     
     override func loadView() {
         super.loadView()
         
+//        locationManager = CLLocationManager()
+        
+        mapView.mapType = .hybrid
         let segmentedControl = UISegmentedControl(items: ["Standard", "Satellite", "Hybrid"])
         segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
-        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.selectedSegmentIndex = 2
         segmentedControl.translatesAutoresizingMaskIntoConstraints = false
         mapView.addSubview(segmentedControl)
         
@@ -38,6 +44,12 @@ class MapViewController: UIViewController, UIGestureRecognizerDelegate, MKMapVie
         
         latLonLabel.alpha = 0.0
         mgrsLabel.alpha = 0.0
+        
+        showLocationButton.layer.cornerRadius = 0.5 * showLocationButton.bounds.size.width
+        showLocationButton.clipsToBounds = true
+        
+        clearMapButton.layer.cornerRadius = 0.5 * clearMapButton.bounds.size.width
+        clearMapButton.clipsToBounds = true
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -52,12 +64,31 @@ class MapViewController: UIViewController, UIGestureRecognizerDelegate, MKMapVie
         }
     }
     
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        let coord = userLocation.coordinate
+        let region = MKCoordinateRegion(center: coord, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        mapView.setRegion(region, animated: true)
+        updateLabels(lat: coord.latitude, lon: coord.longitude)
+        mapView.removeAnnotation(mapPin)
+    }
+    
+    @IBAction func centerViewOnUserPosition(_ sender: UIButton) {
+//        locationManager.startUpdatingLocation()
+        mapView.showsUserLocation = true
+    }
+    
+    @IBAction func clearMapMarks(_ sender: UIButton) {
+        mapView.showsUserLocation = false
+        mapView.removeAnnotation(mapPin)
+    }
+    
     @objc func mapTypeChanged(_ segControl: UISegmentedControl){
         mapView.mapType = MKMapType.init(rawValue: UInt(segControl.selectedSegmentIndex)) ?? .standard
     }
     
     @IBAction func captureUserPressedLocation(sender: UILongPressGestureRecognizer){
         if sender.state != UIGestureRecognizerState.began {return}
+        mapView.showsUserLocation = false
         let touchLoc = sender.location(in: mapView)
         let locCoord = mapView.convert(touchLoc, toCoordinateFrom: mapView)
 //        print("Tapped \(locCoord.latitude), \(locCoord.longitude)")
